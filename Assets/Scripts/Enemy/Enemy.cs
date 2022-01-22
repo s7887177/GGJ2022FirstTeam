3cba93be006c6ee3244ddf60693058a241168ed7
bogay <pojay11523@gmail.com>
2022-01-22 19:26:51 +0800

    Implement enemy wander state

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [field: SerializeField]
+    public EnemyStat stat { get; private set; }
+
+    private IState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.state = new Wander(this, 0, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.state.Update(Time.deltaTime);
+    }
+
+    public void Move(Vector2 delta)
+    {
+        var flip = delta.x < 0;
+        delta = delta.normalized;
+        transform.eulerAngles = new Vector3(0, flip ? 180 : 0, 0);
+        GetComponent<Rigidbody2D>().velocity = delta;
+    }
+}
