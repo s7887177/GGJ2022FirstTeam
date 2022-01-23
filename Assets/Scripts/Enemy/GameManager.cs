a12c0b6637bfbbf437d9efaf0b6cf9eee454e7e7
Yi-feng Li <s7887177@gmail.com>
2022-01-23 11:13:55 +0800

    cc

---
+++ Assets/Scripts/Enemy/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using UnityEngine;
+namespace Feng
+{
+    public class GameManager : MonoBehaviour
+    {
+        public int enemyCount
+        {
+            get
+            {
+                return GameObject.FindObjectsOfType<Enemy>().Count();
+            }
+        }
+        private bool isWin;
+
+        private void Update()
+        {
+            if (isWin) return;
+            if (enemyCount <= 0)
+            {
+                isWin = true;
+                OnWin();
+            }
+        }
+        public void OnWin()
+        {
+            var flowCharts = GameObject.FindObjectsOfType<Fungus.Flowchart>();
+            foreach (var flowchart in flowCharts)
+            {
+                flowchart.ExecuteBlock("Win");
+            }
+        }
+
+    }
+}
